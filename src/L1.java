8efbe09ca8aa461075c8cfc946ddd0dff7d1de35
yateskha000 <yateskha000@warren.k12.in.us>
2021-08-04 11:51:30 -0400

    Completed L1 and L2 notes

--- src/L1.java
+++ src/L1.java
@@ -1,8 +1,7 @@
 public class L1 {
 
-    //You will notice that the class header is already define.
-
-    //add the main method header below.
-
-
+    public static void main(String[] args){
+        System.out.println("Khalil ");
+    }
 }
+
--- src/L2.java
+++ src/L2.java
@@ -1,4 +1,18 @@
 public class L2 {
 
-    //add the main method header
+    //COMMENTS
+    /* This is a block comment.
+    it spans across many lines
+     */
+
+    public static void main(String[] args){
+    String customerName = "George";
+    int bankPin = 1645;
+    double bankBalance= 35.36;
+
+    System.out.println(customerName.toString());
+    System.out.println(bankBalance);
+    System.out.println(bankPin);
+
+    }
 }
