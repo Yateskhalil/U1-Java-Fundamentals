c4ec804e506d23f8d2f3c8d93a773b0cc9ecdc90
yateskha000 <yateskha000@warren.k12.in.us>
2021-08-26 12:23:51 -0400

    Completed L8 and L9 notes

--- src/L9.java
+++ src/L9.java
@@ -1,3 +1,45 @@
+import javax.swing.JOptionPane;
+
+
 public class L9{
 
+    public static void main(String[] args){
+
+        String firstName;
+        String lastName;
+
+        firstName= JOptionPane.showInputDialog("What is your name?");
+        lastName = JOptionPane.showInputDialog("What is your last Name?");
+
+        JOptionPane.showMessageDialog(null,"Hello " + firstName + " " + lastName + "!!");
+
+        //ALL INPUT IS STRING DATA
+
+      int mph, minutes;
+        double hours, distance;
+
+        mph=Integer.parseInt(JOptionPane.showInputDialog("What was your input speed?")) ;
+        minutes=Integer.parseInt(JOptionPane.showInputDialog("How long did,IN MINUTES, you travel?"));
+
+        hours= minutes/60.0;
+
+        distance = hours * mph;
+
+        JOptionPane.showMessageDialog(null,"you travled "+ distance+ " miles");
+
+        //PROTIP: Seprate like data type varaibles on one line with a coma
+
+
+
+        System.exit(0);
+
+
+
+
+
+
+
+
+
+    }
 }
